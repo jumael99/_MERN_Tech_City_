15b16fff370b42a045b554b439b582ce5b8189c4
Jumael Ahmed Khan <jumael99@gmail.com>
2024-07-09 10:33:27 +0600

    redux applied with product screen
    - productScreen working
    - all working with redux
    - we can select how many products are instock
    - next work is to apply add to card option
    - 60% work should be done fast

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,41 +1,108 @@
-import { useEffect, useState } from "react";
-import React from 'react';
-import {Link, useParams} from "react-router-dom";
-import product from '../components/Product'
-import axios from 'axios';
 
-/*here we need to show product details
-* we've a product object need to show those objects elements*/
-function ProductScreen(props) {
-    const [product, setProduct] = useState({});
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { Form,Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import Rating from '../components/Rating';
+import {useGetProductDetailsQuery} from "../slices/productsApiSlice";
 
-    const {id: productId} = useParams();
+const ProductScreen = () => {
+    const { id: productId } = useParams();
 
-    /*If the product id changes we want this to run*/
-    useEffect(() => {
-        const fetchProduct = async () => {
-            const { data } = await axios.get(`http://localhost:5000/api/products/${productId}`);
-            setProduct(data);
-        };
-        fetchProduct();
-    }, [productId]);
+    const [qty, seQty] = useState(1);
+
+    const {data: product, isLoading, error} = useGetProductDetailsQuery(productId);
 
 
-    if (!product) {
-        return <div>Product not found</div>;
-    }
     return (
         <>
-            <Link to="/">
-                Go back
+            <Link to='/' className='btn btn-light my-3'>
+                Go Back
             </Link>
-            <img src={product.image} alt={product.name} className="h-[100px]"/>
-            <h3> {product.name}</h3>
-            <div>rating: {product.rating}</div>
-            <div>review: {product.numReviews}</div>
-            <div>Price: {product.price}</div>
+            {
+                isLoading ? (
+                    <div>load</div>
+                ) : error ? (
+                    <div>{error}</div>
+                ) : (
+                    <Row>
+                <Col md={5}>
+                    <Image src={product.image} alt={product.name} fluid />
+                </Col>
+                <Col md={3}>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <h3>{product.name}</h3>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Rating
+                                value={product.rating}
+                                text={`${product.numReviews} reviews`}
+                            />
+                        </ListGroup.Item>
+                        <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+                        <ListGroup.Item>Description: {product.description}</ListGroup.Item>
+                    </ListGroup>
+                </Col>
+                <Col md={4}>
+                    <Card>
+                        <ListGroup variant='flush'>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Price:</Col>
+                                    <Col>
+                                        <strong>${product.price}</strong>
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Status:</Col>
+                                    <Col>
+                                        {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+
+                            { product.countInStock > 0 && (
+                                <ListGroup.Item>
+                                    <Row>
+                                        <Col>Qty</Col>
+                                        <Col>
+                                            <Form.Control
+                                                as="select"
+                                                value={qty}
+                                                onChange={e => seQty(Number(e.target.value))}
+                                            >
+                                                {[...Array(product.countInStock).keys()].map(x => (
+                                                    <option key={x + 1} value={x + 1}>
+                                                        {x + 1}
+                                                    </option>
+                                                ))}
+                                            </Form.Control>
+                                        </Col>
+                                    </Row>
+                                </ListGroup.Item>
+                            ) }
+
+                            <ListGroup.Item>
+                                <Button
+                                    className='btn-block'
+                                    type='button'
+                                    disabled={product.countInStock === 0}
+                                >
+                                    Add To Cart
+                                </Button>
+                            </ListGroup.Item>
+                        </ListGroup>
+                    </Card>
+                </Col>
+            </Row>
+                )   
+            }
         </>
     );
-}
+};
 
 export default ProductScreen;

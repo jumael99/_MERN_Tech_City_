65d254a9cfa50ccd3f62eeca08ff07f18b5a947a
Jumael Ahmed Khan <jumael99@gmail.com>
2024-07-12 17:20:35 +0600

    added jwt token in auth

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,19 +1,33 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from '../models/userModel.js'
 import * as console from "node:console";
+import jwt from "jsonwebtoken";
 
 // @desc    Auth user & get token
 // @route   POST /api/users/login
 // @access  Public
 const authUser = asyncHandler( async (req,res) => {
     const {email, password} = req.body;
+
     const user = await User.findOne({email} );
+
     if (user && (await user.matchPassword(password))) {
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+            expiresIn: '1ds'
+        });
+
+        // Set JWT as HTTP-Only cookie
+        res.cookie('jwt', token, {
+            httpOnly:true,
+            secure: process.env.NODE_ENV !== 'development',
+            maxAge: 24 * 60 * 60 * 1000
+        })
+
         res.json({
             _id:user._id,
             name:user.name,
-            email:user.email
-
+            email:user.email,
+            isAdmin: user.isAdmin
         })
     } else {
         res.status(401);
